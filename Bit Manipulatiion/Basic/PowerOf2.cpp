5203c9dc18d68ced7b17a1d2e95229cf968bad8c
Suranjana Roy <roysupra75@gmail.com>
2024-09-29 14:04:08 +0530

    ...

---
+++ Bit Manipulatiion/Basic/PowerOf2.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        bool check(int num){
+            if(num<=0) return false;
+            return ((num&(num-1))==0);
+        }
+};
+
+int main()
+{
+    int num;
+    cout << "Enter the num = ";
+    cin >> num;
+    Solution sol;
+    bool ans = sol.check(num);
+    cout << "The entered number is a power of 2: " << ((ans) ? "TRUE" : "FALSE");
+    return 0;
+}
