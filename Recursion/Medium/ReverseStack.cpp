ccf949e9d31274ce28c11cc9545829792cf0a57f
Suranjana Roy <roysupra75@gmail.com>
2024-10-27 11:05:15 +0530

    ...

---
+++ Recursion/Medium/ReverseStack.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertAtBottom(stack<int>&st,int num){
+    if(st.empty()){
+        st.push(num);
+    }else{
+        int top = st.top();
+        st.pop();
+        insertAtBottom(st,num);
+        st.push(top);
+    }
+}
+
+void reverseStack(stack<int>&st){
+    if(st.empty()){
+        return;
+    }
+    int top = st.top();
+    st.pop();
+    reverseStack(st);
+    insertAtBottom(st,top);
+}
+
+
+int main()
+{
+    int n;
+    cout << "Enter the stack size = ";
+    cin >> n;
+    stack<int> st;
+    cout << "Enter the elements: " << endl;
+    for(int i=1;i<=n;i++){
+        int val;
+        cin >> val;
+        st.push(val);
+    }
+
+    reverseStack(st);
+    cout << "After reversing the stack becomes: " << endl;
+    while(!st.empty()){
+        cout << st.top() << endl;
+        st.pop();
+    }
+    return 0;
+}
